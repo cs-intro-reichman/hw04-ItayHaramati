ac655628bd8fbee5abeb880c25f0ffbec434245e
Itay <you@example.com>
2024-01-16 20:03:38 +0200

    I changed StringOps

--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,97 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println(capVowelsLowRest("Leo Messi is the best footballer ever"));
+        int[] arr = allIndexOf( "Hello world" ,'l');
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+    public static String capVowelsLowRest (String string) {
+        String newStr="";
+        for(int i=0; i<string.length(); i++)
+        {
+            char ch = string.charAt(i);
+            if((ch==97)||(ch==101)||(ch==105)||(ch==111)||(ch==117)) //upper (a,e,i,o,u)
+            {
+                newStr=newStr+(char)(string.charAt(i)-32);
+            }
+            else
+            {
+                if((ch>=64)&&(ch<=91))
+            {
+                newStr=newStr+(char)(string.charAt(i)+32);
+            }
+            else{
+                newStr=newStr+(char)(string.charAt(i));
+            }
+            }
+        }
+        return newStr;
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String newword = "";
+
+        //// Changing the first letter to small letter
+            char first = string.charAt(0);
+            int num = (int) first;
+            if (num >= 65 && num <= 90) {
+                num = num + 32;
+                char e = (char) num;
+                newword = newword + e;
+            } else {
+                newword = newword + first;}
+        
+        //// Changing the first letter of each word after the first word to capital letter,
+        /// erasing all spaces, and converting other letters to small letters
+            boolean cap = false;
+        
+            for (int i = 1; i < string.length(); i++) {
+                char currentchar = string.charAt(i);
+                int num1 = (int) currentchar;
+                if (num1 == 32) {
+                    cap = true;
+                } else {
+                    if (cap) {
+                        if (num1 >= 97 && num1 <= 122) {
+                            num1 = num1 - 32;
+                        }
+                        char con = (char) num1;
+                        newword = newword + con;
+                        cap = false;
+                    } else {
+        /// other letter to small letters
+                        if (num1 >= 65 && num1 <= 90) {
+                            num1 = num1 + 32;}
+                        newword = newword + (char) num1;
+                    }
+                }
+            }
+            return newword;
+        }
+        
+
+    public static int[] allIndexOf (String str, char chr) {
+        int arraycount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == chr)
+                arraycount++;
+        }
+
+        int[] arr = new int[arraycount];
+        arraycount = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == chr) {
+                arr[arraycount] = i;
+                arraycount++;
+            }
+        }
+        return arr;
+
     }
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
     }
-}
+
